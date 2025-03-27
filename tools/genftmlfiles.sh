99e051e858d1ef05e6d0ebb491b16eaa0c0d0498
Lorna Evans <LornaSIL@users.noreply.github.com>
2025-03-27 15:05:56 -0500

    Update ftml generation. [nobuild]

--- tools/genftmlfiles.sh
+++ tools/genftmlfiles.sh
@@ -29,11 +29,11 @@ commonParams=( \
 	-w 75%  \
 #	--ucdxml source/additional_ucd.xml  \
 #	-s "url(../references/v1.100/EASTSM-Regular.ttf)|ref"  \
-	-s "url(../results/SyriacProto-Regular.ttf)|Reg" \
+#	-s "url(../results/SyriacProto-Regular.ttf)|Reg" \
 )
 
 echo "Rebuilding ftml files..."
-# tools/psfgenftml.py -q -t 'AllChars (auto)'                      source/masters/SyriacProto-Regular.ufo  tests/AllChars-auto.ftml        -l tests/logs/AllChars.log         "${commonParams[@]}" -s 'url(../results/SyriacProto-Light.ttf)|Lt' -s 'url(../results/SyriacProto-Regular.ttf)|Reg' &
+tools/psfgenftml.py -q -t 'AllChars (auto)'                      source/masters/SyriacProto-Regular.ufo  tests/AllChars-auto.ftml        -l tests/logs/AllChars.log         "${commonParams[@]}" -s 'url(../results/SyriacProto-Light.ttf)|Lt' -s 'url(../results/SyriacProto-Regular.ttf)|Reg' &
 tools/psfgenftml.py -q -t 'Diac Short (auto)'                    source/masters/SyriacProto-Regular.ufo  tests/Diac-short-auto.ftml      -l tests/logs/DiacTest1-short.log  "${commonParams[@]}" -s 'url(../results/SyriacProto-Light.ttf)|Lt' -s 'url(../results/SyriacProto-Regular.ttf)|Reg' &
 tools/psfgenftml.py -q -t 'Diac (auto)'                          source/masters/SyriacProto-Regular.ufo  tests/Diac-auto.ftml            -l tests/logs/DiacTest1.log        "${commonParams[@]}" -s 'url(../results/SyriacProto-Light.ttf)|Lt' & 
 
