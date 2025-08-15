2fbb26764e16e70ef36be721afb61a8bee4df56a
Bobby de Vos <bobby_devos@sil.org>
2025-08-15 09:49:59 -0600

    Re-import the Latin glyphs for the ExtraLight master
    And some glyphs for the Regular master as well.

--- tools/archive/latin/import.bash
+++ tools/archive/latin/import.bash
@@ -2,22 +2,23 @@
 
 ar=$PWD/tools/archive/latin
 glyphnames=$HOME/script/smithplus/etc/glyph_names/glyph_names.csv
-import=$HOME/script/syrc/fonts/idiqlatsyriac_proto-local/latin/Lateef
+import=$HOME/script/syrc/fonts/idiqlat-local/latin/Lateef
 
 pushd source/masters
-for weight in Light Regular
+for weight in ExtraLight Regular
 do
     ufo=Idiqlat-${weight}.ufo
 
     # prepare
-    psfdeleteglyphs -i $ar/delete.txt $ufo
+    # psfdeleteglyphs -i $ar/delete.txt $ufo
+    psfdeleteglyphs -i $ar/sorts_latin.txt $ufo
 
     # import
     scale="--scale 0.875" # 10.5 / 12
     glyphs=$ar/import-${weight}.csv
     latin=${import}-${weight}.ufo
     # psfgetglyphnames -i $ar/import.txt -a $glyphnames $latin $glyphs
-    psfcopyglyphs --rename new_name --unicode usv $scale -s $latin -i $ar/latin_import.csv -l ${weight}.log $ufo
+    psfcopyglyphs -f --rename new_name --unicode usv $scale -s $latin -i $ar/latin_import.csv -l ${weight}.log $ufo
 
     # cleanup
     # $HOME/script/tools/fix-spaces.py $ufo
@@ -27,8 +28,10 @@ do
     ls -l $ufo/glyphs/*copy?.glif
 
     # colors
-    psfsetmarkcolors -i $ar/import.txt -u -c g_light_gray $ufo
-    cut -d\, -f 2 $ar/latin_import.csv  | grep .latn > $ar/color_latin.txt
+    # psfsetmarkcolors -i $ar/import.txt -u -c g_light_gray $ufo
+    cut -d\, -f 1 $ar/latin_import.csv | uniq > $ar/color_latin.txt
+    cut -d\, -f 2 $ar/latin_import.csv | uniq >> $ar/color_latin_rename.txt
+    psfsetmarkcolors -i $ar/color_latin_rename.txt -c g_light_gray $ufo
     psfsetmarkcolors -i $ar/color_latin.txt -c g_light_gray $ufo
     psfsetmarkcolors -i $ar/sorts_latin.txt -c g_light_gray $ufo
 done
